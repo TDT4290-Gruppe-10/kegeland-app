67cbdbd4480c94d5c0f1e9ad42dc369570a97da4
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-09-21 00:38:13 +0200

    Add constants for bluetooth and permissions

---
+++ app/src/constants/bluetooth.ts
@@ -0,0 +1,2 @@
+export const SCAN_TIME = 10;
+export const ALLOW_DUPLICATE_DEVICES = false;
---
+++ app/src/constants/permissions.ts
@@ -0,0 +1,4 @@
+export const ANDROID_PERMISSIONS: string[] = [
+  'ACCESS_FINE_LOCATION',
+  'BLUETOOTH_SCAN',
+];
