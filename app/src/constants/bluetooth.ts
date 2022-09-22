eaa37faca11d241d1fbbebaa8d59fcc42872d3eb
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-09-22 02:36:57 +0200

    Add constants for bluetooth

--- app/src/constants/bluetooth.ts
+++
@@ -1,2 +0,0 @@
-export const SCAN_TIME = 10;
-export const ALLOW_DUPLICATE_DEVICES = false;
---
+++ app/src/constants/bluetooth/index.ts
@@ -0,0 +1,13 @@
+import {FEMFIT_PROFILE} from './peripherals/femfit';
+
+import {BluetoothProfile} from './interface';
+
+export const SCAN_TIME = 3;
+export const ALLOW_DUPLICATE_DEVICES = false;
+export const UPDATE_INTERVAL_MS = 500;
+
+export const BLE_PROFILES: Record<string, BluetoothProfile> = {
+  femfit: FEMFIT_PROFILE,
+};
+
+export type ProfileKey = keyof typeof BLE_PROFILES;
