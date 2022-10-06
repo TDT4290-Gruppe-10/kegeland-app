10f98901bbe50c05cd0d613b0c245f50e3ef95a9
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-10-06 07:10:08 +0200

    Fixed import inconsistency

--- app/src/components/Section.tsx
+++ app/src/components/Section.tsx
@@ -7,7 +7,7 @@ type SectionProps = {
   children?: React.ReactNode;
 };
 
-export const Section: React.FC<SectionProps> = ({title, children}) => {
+const Section: React.FC<SectionProps> = ({title, children}) => {
   const {colors} = useTheme();
   return (
     <Surface style={styles.surface}>
