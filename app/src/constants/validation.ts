fe0308fc80919bf17947296bbe00f1f85ca85a4f
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-10-09 17:46:41 +0100

    Removed unused code

--- app/src/constants/validation.ts
+++
@@ -1,6 +0,0 @@
-export const MESSAGE = {
-  REQUIRED: 'This Field Is Required',
-  NAME_INVALID: 'Not a Valid Name',
-  TERMS: 'Terms Must Be Accepted To Continue',
-  EMAIL_INVALID: 'Not a Valid Email',
-};
