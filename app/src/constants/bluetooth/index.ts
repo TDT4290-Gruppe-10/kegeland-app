20a9eeba8131f67d0248c1721289070f5cf89f26
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-10-09 17:44:56 +0100

    Changed default scan-time for bluetooth

--- app/src/constants/bluetooth/index.ts
+++ app/src/constants/bluetooth/index.ts
@@ -2,7 +2,7 @@ import {FEMFIT_PROFILE} from './peripherals/femfit';
 
 import {BluetoothProfile} from './interface';
 
-export const SCAN_TIME = 3;
+export const SCAN_TIME = 5;
 export const ALLOW_DUPLICATE_DEVICES = false;
 export const UPDATE_INTERVAL_MS = 500;
 
