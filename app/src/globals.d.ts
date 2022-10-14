ab85775f7eeada5808a3f47bfddd48369caf713e
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-10-14 03:56:17 +0100

    Refactored global type-defs

--- app/src/globals.d.ts
+++ app/src/globals.d.ts
@@ -1,7 +1,12 @@
+import {ThemeOverride} from '~constants/theme';
 import {RootTabParamList} from '~routes/interface';
 
 declare global {
   namespace ReactNavigation {
     interface RootParamList extends RootTabParamList {}
+  }
+
+  namespace ReactNativePaper {
+    interface ThemeColors extends ThemeOverride {}
   }
 }
