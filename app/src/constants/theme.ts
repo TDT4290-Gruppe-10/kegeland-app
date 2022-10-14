835d56d5baa71022c8135dab65ada5bb62f92ace
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-10-14 03:58:13 +0100

    Improved theme-overrides

--- app/src/constants/theme.ts
+++ app/src/constants/theme.ts
@@ -1,29 +1,64 @@
-import {merge} from 'lodash';
+import {cloneDeep, merge} from 'lodash';
 import {
   DarkTheme as NavigationDarkTheme,
   DefaultTheme as NavigationLightTheme,
 } from '@react-navigation/native';
 import {
-  MD3DarkTheme as PaperDarkTheme,
+  DarkTheme as PaperDarkTheme,
   DefaultTheme as PaperLightTheme,
+  Theme,
 } from 'react-native-paper';
 
-const defaultLightTheme = merge(NavigationLightTheme, PaperLightTheme);
-const defaultDarkTheme = merge(PaperDarkTheme, NavigationDarkTheme);
+export type ThemeOverride = Theme['colors'] & {
+  muted: string;
+  elevation: {
+    elevation1: string;
+  };
+  status: {
+    success: string;
+    alert: string;
+    error: string;
+  };
+};
 
-type Theme = typeof defaultLightTheme;
+const baseOverride: Partial<Omit<Theme, 'colors'>> = {
+  roundness: 15,
+};
 
-export const lightTheme: Theme = {
-  ...defaultLightTheme,
-  roundness: 3,
-  colors: {
-    ...defaultLightTheme.colors,
-    primary: '#00b0ff', // '#D25660',
-    text: '#333',
+const sharedColorOverride: Partial<ThemeOverride> = {
+  primary: '#00b0ff',
+  status: {
+    success: '#64dd17',
+    alert: '#ffd600',
+    error: '#dd2c00',
   },
 };
-export const darkTheme: Theme = {
-  ...defaultDarkTheme,
-  roundness: 3,
-  colors: {...defaultDarkTheme.colors, primary: '#00b0ff'},
-};
+
+const lightColorOverride: Partial<Theme['colors']> = merge(
+  cloneDeep(sharedColorOverride),
+  {
+    muted: 'rgba(0, 0, 0, 0.54)',
+    elevation: {
+      elevation1: '#fff',
+    },
+  } as ThemeOverride,
+);
+
+const darkColorOverride: Partial<Theme['colors']> = merge(
+  cloneDeep(sharedColorOverride),
+  {
+    muted: 'rgba(229, 229, 231, 0.54)',
+    elevation: {
+      elevation1: '#272727',
+    },
+  } as ThemeOverride,
+);
+
+export const lightTheme = merge(merge(NavigationLightTheme, PaperLightTheme), {
+  ...baseOverride,
+  colors: lightColorOverride,
+});
+export const darkTheme = merge(merge(PaperDarkTheme, NavigationDarkTheme), {
+  ...baseOverride,
+  colors: darkColorOverride,
+});
