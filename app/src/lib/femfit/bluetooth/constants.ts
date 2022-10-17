73af114463ba187f2ea6806a8324315946c37f70
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-10-17 07:58:05 +0100

    Remove broken sensor characteristic - DEBUG

--- app/src/lib/femfit/bluetooth/constants.ts
+++ app/src/lib/femfit/bluetooth/constants.ts
@@ -2,7 +2,7 @@ import {BluetoothService} from '~constants/bluetooth/interface';
 
 export const PRESSURE_SENSOR_MIN_VAL = 760;
 export const PRESSURE_SENSOR_MAX_VAL = 970;
-export const ACTIVATION_THRESHOLD = 0.05;
+export const ACTIVATION_THRESHOLD = 0.15;
 
 export const SERVICES = {
   HOUSEKEEPING_SERVICE: 'b92a0001-4bf9-4870-8aa1-881b3a20ada4',
@@ -18,7 +18,7 @@ export const SENSOR_SERVICE: BluetoothService = {
   uuid: '0d9e0001-c111-49cd-bba3-85c7471cb6fa',
   characteristics: [
     '0d9e0002-c111-49cd-bba3-85c7471cb6fa',
-    '0d9e0003-c111-49cd-bba3-85c7471cb6fa',
+    // '0d9e0003-c111-49cd-bba3-85c7471cb6fa',
     '0d9e0004-c111-49cd-bba3-85c7471cb6fa',
     '0d9e0005-c111-49cd-bba3-85c7471cb6fa',
   ],
