a46d4c292510071d38ad8a7f21cc60e951c603a1
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-10-17 19:29:28 +0200

    Approximated number of coins per second

--- app/src/lib/femfit/game/constants.ts
+++ app/src/lib/femfit/game/constants.ts
@@ -4,7 +4,7 @@ export default {
   MAX_WIDTH: Dimensions.get('screen').width,
   MAX_HEIGHT: Dimensions.get('screen').height,
   BASELINE: Dimensions.get('screen').height * 0.6,
-  STARS_PER_SEC: 4,
+  COINS_PER_SEC: 1,
   HEADER_HEIGHT: 50,
   BOUNDARY_HEIGHT: 50,
   SPRITE_SIZE: 128,
