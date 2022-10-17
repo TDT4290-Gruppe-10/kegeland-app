bdb8dbd23fe4d7fb26ecc43b5b3a5dd0e05119f3
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-10-17 07:59:53 +0100

    Remove game from HomeScreen

--- app/src/views/HomeScreen.tsx
+++ app/src/views/HomeScreen.tsx
@@ -1,22 +1,11 @@
-import {find} from 'lodash';
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
-
-import FemfitGame from '~lib/femfit/game';
-import useAppSelector from '~hooks/useAppSelector';
+import {Text} from 'react-native-paper';
 
 const HomeScreen: React.FC = () => {
-  const device = useAppSelector((state) =>
-    find(
-      state.bluetooth.connectedDevices,
-      (device) => device.type === 'femfit',
-    ),
-  );
-
   return (
     <SafeAreaView style={styles.wrapper}>
-      {device && <FemfitGame device={device} />}
-      {/* {device && <FemfitDebugger device={device} />} */}
+      <Text>Halla</Text>
     </SafeAreaView>
   );
 };
