dd60fc5115f9cb9875a3fdd11e7475de91eefe63
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-10-17 07:56:47 +0100

    Refactor game-entities

--- app/src/lib/femfit/game/entities/Boundary.tsx
+++ app/src/lib/femfit/game/entities/Boundary.tsx
@@ -1,28 +1,25 @@
 import React from 'react';
 import Matter from 'matter-js';
-import {Image} from 'react-native';
+import {View} from 'react-native';
 
-import CoinSprite from '~assets/femfit/gold.png';
-
-import {EntityBase, Position} from '../interface';
 import constants from '../constants';
+import {EntityBase, Position} from '../interface';
 
 import styles from './styles';
 
-const {COIN_SIZE} = constants;
+const {MAX_WIDTH, BOUNDARY_HEIGHT} = constants;
 
-export type CoinEntity = EntityBase & {
-  scored: boolean;
-};
+type BoundaryProps = EntityBase;
 
-class Coin extends React.Component<CoinEntity> {
+class Boundary extends React.Component<BoundaryProps> {
   render(): React.ReactNode {
     const width = this.props.body.bounds.max.x - this.props.body.bounds.min.x;
     const height = this.props.body.bounds.max.y - this.props.body.bounds.min.y;
     const x = this.props.body.position.x - width / 2;
     const y = this.props.body.position.y - height / 2;
+
     return (
-      <Image
+      <View
         style={[
           styles.entity,
           {
@@ -32,19 +29,23 @@ class Coin extends React.Component<CoinEntity> {
             height,
           },
         ]}
-        resizeMode="stretch"
-        source={CoinSprite}
       />
     );
   }
 }
 
 export default (world: Matter.World, pos: Position, label: string) => {
-  const body = Matter.Bodies.rectangle(pos.x, pos.y, COIN_SIZE, COIN_SIZE, {
-    label,
-    isSensor: true,
-    isStatic: true,
-  });
+  const body = Matter.Bodies.rectangle(
+    pos.x,
+    pos.y,
+    MAX_WIDTH,
+    BOUNDARY_HEIGHT,
+    {
+      label,
+      isStatic: true,
+      density: 1,
+    },
+  );
   Matter.World.add(world, body);
-  return {body, scored: false, renderer: Coin};
+  return {body, renderer: Boundary};
 };
--- app/src/lib/femfit/game/entities/Player.tsx
+++ app/src/lib/femfit/game/entities/Player.tsx
@@ -1,27 +1,26 @@
 import React from 'react';
 import Matter from 'matter-js';
-import {StyleSheet} from 'react-native';
 
-import BirdSpriteSheet from '~assets/femfit/bird.png';
+import PlayerSpriteSheet from '~assets/femfit/bird.png';
 
+import {EntityBase, Position} from '../interface';
 import constants from '../constants';
-import {Position} from '../interface';
 import {SpriteSheet} from '../SpriteSheet';
 
-// type BirdProps = {
-//   body: Matter.Body;
-// };
+import styles from './styles';
 
-class Bird extends React.Component<any> {
+const {PLAYER_SIZE} = constants;
+type PlayerProps = EntityBase;
+
+class Player extends React.Component<PlayerProps> {
   render(): React.ReactNode {
     const width = this.props.body.bounds.max.x - this.props.body.bounds.min.x;
     const height = this.props.body.bounds.max.y - this.props.body.bounds.min.y;
     const x = this.props.body.position.x - width / 2;
     const y = this.props.body.position.y - height / 2;
-
     return (
       <SpriteSheet
-        src={BirdSpriteSheet}
+        src={PlayerSpriteSheet}
         rows={1}
         cols={3}
         anims={[{name: 'default', row: 0, frames: 3}]}
@@ -32,30 +31,13 @@ class Bird extends React.Component<any> {
   }
 }
 
-const styles = StyleSheet.create({
-  entity: {
-    position: 'absolute',
-    aspectRatio: 1,
-    borderColor: 'green',
-    borderWidth: 2,
-  },
-});
-
 export default (world: Matter.World, pos: Position) => {
-  const body = Matter.Bodies.rectangle(
-    pos.x,
-    pos.y,
-    constants.BIRD_WIDTH,
-    constants.BIRD_WIDTH,
-    {
-      label: 'Bird',
-      frictionAir: 0,
-      friction: 0,
-      restitution: 0,
-      inertia: Infinity,
-    },
-  );
+  const body = Matter.Bodies.rectangle(pos.x, pos.y, PLAYER_SIZE, PLAYER_SIZE, {
+    label: 'Player',
+    restitution: 0,
+    density: 0.0025,
+    frictionAir: 0.015,
+  });
   Matter.World.add(world, body);
-
-  return {body, renderer: <Bird />};
+  return {body, renderer: Player};
 };
