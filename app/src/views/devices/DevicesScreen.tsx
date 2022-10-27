5af92135dc0d131c48e18add86610faa19596b23
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-10-27 19:06:09 +0100

    Removed questionnaire logic from device screen

--- app/src/views/devices/DevicesScreen.tsx
+++ app/src/views/devices/DevicesScreen.tsx
@@ -1,8 +1,7 @@
 import {capitalize, clone, forEach, reduce} from 'lodash';
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {ImageSourcePropType, ScrollView, StyleSheet} from 'react-native';
 import {Avatar, Button, List, useTheme} from 'react-native-paper';
-import {useFocusEffect} from '@react-navigation/native';
 
 import {BLE_PROFILES, DeviceType} from '~constants/bluetooth';
 import {DeviceScreenProps} from '~routes/interface';
@@ -10,12 +9,7 @@ import FemfitImage from '~assets/devices/femfit.png';
 import useAppSelector from '~hooks/useAppSelector';
 import PageWrapper from '~components/PageWrapper';
 import {getDeviceScreen} from '~utils/bluetooth';
-import QuestionnaireModal from '~components/QuestionnaireModal';
-import useAppDispatch from '~hooks/useAppDispatch';
-import {
-  clearAnswers,
-  setAnswer,
-} from '~state/ducks/questions/questions.reducer';
+
 const deviceTypes = Object.keys(BLE_PROFILES);
 
 const imgMap: Record<DeviceType, ImageSourcePropType> = {
@@ -34,28 +28,10 @@ const initialDeviceMap = reduce(
 const DevicesScreen: React.FC<DeviceScreenProps<'Devices'>> = ({
   navigation,
 }) => {
-  const dispatch = useAppDispatch();
   const {colors, roundness} = useTheme();
-  const {session} = useAppSelector((state) => state.session);
-  const {authUser} = useAppSelector((state) => state.auth);
-  const {questionnaire, answers} = useAppSelector((state) => state.questions);
   const [devices, setDevices] =
     useState<Record<DeviceType, boolean>>(initialDeviceMap);
   const {connectedDevices} = useAppSelector((state) => state.bluetooth);
-  const [showQuestionnaire, setShowQuestionnaire] = useState<boolean>(false);
-
-  useFocusEffect(
-    useCallback(() => {
-      if (session && questionnaire) {
-        if (answers.length === 1) {
-          setShowQuestionnaire(true);
-        }
-      }
-      return () => {
-        dispatch(clearAnswers());
-      };
-    }, [session, questionnaire]),
-  );
 
   useEffect(() => {
     const tmp = clone(initialDeviceMap);
@@ -64,22 +40,6 @@ const DevicesScreen: React.FC<DeviceScreenProps<'Devices'>> = ({
     });
     setDevices(tmp);
   }, [connectedDevices]);
-
-  useEffect(() => {
-    if (session && questionnaire) {
-      if (answers.length === 2) {
-        dispatch(clearAnswers());
-        console.log('upload session data');
-      }
-    }
-  }, [session, answers, questionnaire]);
-
-  const handleAnswers = (data: number[]) => {
-    setShowQuestionnaire(false);
-    if (authUser) {
-      dispatch(setAnswer({userId: authUser.id, answers: data}));
-    }
-  };
 
   return (
     <PageWrapper title="Select device" contentSize="medium">
@@ -117,11 +77,6 @@ const DevicesScreen: React.FC<DeviceScreenProps<'Devices'>> = ({
           />
         ))}
       </ScrollView>
-      <QuestionnaireModal
-        onSubmit={handleAnswers}
-        visible={showQuestionnaire}
-        questionnaire={questionnaire}
-      />
     </PageWrapper>
   );
 };
