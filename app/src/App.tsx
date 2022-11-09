f3e31476dfbe26d33618468bbf4666e264ac5066
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-11-09 03:49:10 +0100

    Added conditional persisted redux store

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,23 +3,35 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {Provider as ReduxProvider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 
-import {store, persistor} from '~state/store';
+import {store as persistedStore, persistor} from '~state/store';
 import withAppWrapper from '~hoc/withAppWrapper';
 import withThemedNavigation from '~hoc/withThemedNavigation';
+import {mockStore} from '~state/ducks/__mocks__/store.mock';
 
 import Router from './routes/Router';
 
 const Wrapper = withAppWrapper(SafeAreaProvider);
 const ThemedRouter = withThemedNavigation(Router);
 
+const isTest = process.env.NODE_ENV === 'test';
+
+const store = isTest ? mockStore() : persistedStore;
+
 export default function App() {
+  const content = () => (
+    <Wrapper>
+      <ThemedRouter />
+    </Wrapper>
+  );
   return (
     <ReduxProvider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Wrapper>
-          <ThemedRouter />
-        </Wrapper>
-      </PersistGate>
+      {isTest ? (
+        content()
+      ) : (
+        <PersistGate loading={null} persistor={persistor}>
+          {content()}
+        </PersistGate>
+      )}
     </ReduxProvider>
   );
 }
