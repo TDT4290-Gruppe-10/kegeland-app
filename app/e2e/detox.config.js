21c872e142391f3602b328016bf14c4f73ec4423
Simen Kristoffersen <simekri@stud.ntnu.no>
2022-11-09 19:10:00 +0000

    Removed headless flag and added detox-cli

--- app/e2e/detox.config.js
+++ app/e2e/detox.config.js
@@ -3,7 +3,7 @@ module.exports = {
   maxWorkers: 1,
   testEnvironment: './environment',
   testRunner: 'jest-circus/runner',
-  testTimeout: 120000,
+  testTimeout: 180000,
   testRegex: '\\.e2e\\.ts$',
   reporters: ['detox/runners/jest/streamlineReporter'],
   verbose: true,
