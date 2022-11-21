1b31d67dc4551513267b00c371fe52d1c74812af
Håkon Bygdås Høgset <hakonbyh@gmail.com>
2022-11-21 17:16:18 +0100

    halla

--- app/src/lib/femfit/bluetooth/constants.ts
+++ app/src/lib/femfit/bluetooth/constants.ts
@@ -1,34 +1,44 @@
 import {BluetoothService} from '~constants/bluetooth/interface';
+import {transformId} from '~utils/platform.utils';
 
 export const PRESSURE_SENSOR_MIN_VAL = 760;
 export const PRESSURE_SENSOR_MAX_VAL = 970;
 export const ACTIVATION_THRESHOLD = 0.15;
 
 /**
+ * Transform UUIDs based on the current platform
+ */
+const HOUSEKEEPING_SERVICE_UUID = transformId(
+  'b92a0001-4bf9-4870-8aa1-881b3a20ada4',
+);
+const HOUSEKEEPING_CHARACTERISTIC_UUID = transformId(
+  'b92a0002-4bf9-4870-8aa1-881b3a20ada4',
+);
+const SENSOR_SERVICE_UUID = transformId('0d9e0001-c111-49cd-bba3-85c7471cb6fa');
+const SENSOR_1_UUID = transformId('0d9e0002-c111-49cd-bba3-85c7471cb6fa');
+const SENSOR_2_UUID = transformId('0d9e0003-c111-49cd-bba3-85c7471cb6fa');
+const SENSOR_3_UUID = transformId('0d9e0004-c111-49cd-bba3-85c7471cb6fa');
+const SENSOR_4_UUID = transformId('0d9e0005-c111-49cd-bba3-85c7471cb6fa');
+/**
  * The femfit services
  */
 export const SERVICES = {
-  HOUSEKEEPING_SERVICE: 'b92a0001-4bf9-4870-8aa1-881b3a20ada4',
-  SENSOR_SERVICE: '0d9e0001-c111-49cd-bba3-85c7471cb6fa',
+  HOUSEKEEPING_CHARACTERISTIC: HOUSEKEEPING_CHARACTERISTIC_UUID,
+  SENSOR_SERVICE: SENSOR_SERVICE_UUID,
 };
 
 /**
  * The housekeeping service for femfit
  */
 export const HOUSEKEEPING_SERVICE: BluetoothService = {
-  uuid: 'b92a0001-4bf9-4870-8aa1-881b3a20ada4',
-  characteristics: ['b92a0002-4bf9-4870-8aa1-881b3a20ada4'],
+  uuid: HOUSEKEEPING_SERVICE_UUID,
+  characteristics: [HOUSEKEEPING_CHARACTERISTIC_UUID],
 };
 
 /**
  * The sensor service for femfit
  */
 export const SENSOR_SERVICE: BluetoothService = {
-  uuid: '0d9e0001-c111-49cd-bba3-85c7471cb6fa',
-  characteristics: [
-    '0d9e0002-c111-49cd-bba3-85c7471cb6fa',
-    '0d9e0003-c111-49cd-bba3-85c7471cb6fa',
-    '0d9e0004-c111-49cd-bba3-85c7471cb6fa',
-    '0d9e0005-c111-49cd-bba3-85c7471cb6fa',
-  ],
+  uuid: SENSOR_SERVICE_UUID,
+  characteristics: [SENSOR_1_UUID, SENSOR_2_UUID, SENSOR_3_UUID, SENSOR_4_UUID],
 };
